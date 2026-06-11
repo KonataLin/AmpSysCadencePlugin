349870eaade1f96ceb366e0aa8abdc9ef2ed986e
WenkangDou2026 <2424441676@qq.com>
2026-06-11 15:26:45 +0800

    Remove protected core from repository

--- core/windows_amd64/ampsys_core/_internal/AmpSys/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-AmpSys Core - 数据结构与验证
-"""
-
-from .intent import MosIntent, PassiveIntent
-from .validator import KCLValidator
-
-__all__ = ['MosIntent', 'PassiveIntent', 'KCLValidator']
--- core/windows_amd64/ampsys_core/_internal/AmpSys/engine/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AmpSys Engine - 综合流程引擎
-"""
-
-from .flow import AmpFlow, AmpFlowConfig
-
-__all__ = ['AmpFlow', 'AmpFlowConfig']
